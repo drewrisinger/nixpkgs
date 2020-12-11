1bbfbbdc702c7c7d16954385ab8be08ac8088180
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-12-11 09:35:39 -0500

    pythonPackages.pytestquickcheck: fix breakage
    * Allow working with pytest >= 6.0
    * Actually run tests: the setuptoolsCheckPhase wasn't running any tests

--- pkgs/development/python-modules/pytest-quickcheck/default.nix
+++ pkgs/development/python-modules/pytest-quickcheck/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi, pytest, pytest-flakes, tox }:
+{ stdenv, buildPythonPackage, fetchPypi, pytest, pytest-flakes, tox, pytestCheckHook }:
 buildPythonPackage rec {
   pname = "pytest-quickcheck";
   version = "0.8.6";
@@ -8,13 +8,16 @@ buildPythonPackage rec {
     sha256 = "3ef9bde7ba1fe6470c5b61631440186d1254e276c67a527242d91451ab7994e5";
   };
 
+  postPatch = "substituteInPlace setup.py --replace 'pytest>=4.0,<6.0.0' 'pytest'";
+
   buildInputs = [ pytest ];
   propagatedBuildInputs = [ pytest-flakes tox ];
+
+  checkInputs = [ pytestCheckHook ];
 
   meta = with stdenv.lib; {
     license = licenses.asl20;
     homepage = "https://pypi.python.org/pypi/pytest-quickcheck";
     description = "pytest plugin to generate random data inspired by QuickCheck";
-    broken = true; # missing pytest-codestyle
   };
 }
