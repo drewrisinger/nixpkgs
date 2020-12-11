4efa9096c19fa84fced5b52da2214ba1a2bcdb53
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-12-11 11:40:00 -0500

    pepper: fix build

--- pkgs/tools/admin/salt/pepper/default.nix
+++ pkgs/tools/admin/salt/pepper/default.nix
@@ -11,16 +11,24 @@ python3Packages.buildPythonApplication rec {
     sha256 = "1wh6yidwdk8jvjpr5g3azhqgsk24c5rlzmw6l86dmi0mpvmxm94w";
   };
 
-  buildInputs = with python3Packages; [ setuptools setuptools_scm salt ];
-  checkInputs = with python3Packages; [
-    pytest mock pyzmq pytest-rerunfailures pytestcov cherrypy tornado
+  buildInputs = with python3Packages; [ setuptools_scm ];
+  propagatedBuildInputs = with python3Packages; [
+    salt
+    setuptools
   ];
+  # Running pytest requires >=2 out-of-Nix packages: pytest-salt, pytest-tempdir
+  # checkInputs = with python3Packages; [
+  #   pytestCheckHook mock pyzmq pytest-rerunfailures pytestcov cherrypy tornado pytestsalt
+  # ];
+  checkPhase = ''
+    $out/bin/pepper --help > /dev/null
+    $out/bin/pepper --version
+  '';
 
   meta = with lib; {
     description = "A CLI front-end to a running salt-api system";
     homepage = "https://github.com/saltstack/pepper";
     maintainers = [ maintainers.pierrer ];
     license = licenses.asl20;
-    broken = true; # ModuleNotFoundError: No module named 'pytestsalt'
   };
 }
